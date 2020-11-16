880300b17e43cc34b8c0ea77aeb47cbe61ca294c
feli03002 <73816572+feli03002@users.noreply.github.com>
2020-11-16 10:42:23 +0100

    Forms version.1

---
+++ Gissa_Tal_Forms/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gissa_Tal_Forms
+{
+    public partial class Form1 : Form
+    {
+        public int tal;
+        public int svar;
+        public int försök = 0;
+        public Random rng = new Random();
+        
+        public Form1()
+        {
+            InitializeComponent();
+            tal = rng.Next(0, 101);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                svar = Convert.ToInt32(textBox1.Text);
+            }
+            catch(FormatException)
+            {
+            }
+            
+            if (svar < tal)
+            {
+                label3.Text = "För låg!";
+                försök++;
+            }
+            else if (svar > tal)
+            {
+                label3.Text = "För högt!";
+                försök++;
+            }
+            else
+                label3.Text = "Rätt!";
+        }
+    }
+}
